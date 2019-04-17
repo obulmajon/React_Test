9c14fa585d475b07a82c6d237393d763be05c5ba
Obulmajon <obulmajons@gmail.com>
2019-04-17 09:52:50 +0500

    Dialog companentasini qo'shdim

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import s from './Profile.module.css';
 import MyPost from './MyPost/MyPost';
 
 const Profile = () => {
-    return <div className={s.content}>
+    return <div>
     <div>
       <img src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350' />
     </div>
